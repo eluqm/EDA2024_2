26f4cbba3ba4b5f876108f793a6fd4cee803d779
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-07-17 18:13:40 -0500

    add Result struct to send data to frontend

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -1,7 +1,8 @@
 package reader
 
 import (
-	invertI "eda/structures/invertIndex"
+	// invertI "eda/structures/invertIndex"
+
 	s "eda/types"
 	"fmt"
 	"testing"
@@ -9,44 +10,18 @@ import (
 )
 
 func TestReadCSV(t *testing.T) {
-	invertIndex := invertI.NewInvertIndex[s.Song](300 * 1000)
-
 	f := func(s *s.Song) {
-		name := s.TrackName
 
-		invertIndex.PutMany(name, s)
 	}
 
-	err := ReadCSV(f, 100)
+	start := time.Now()
+
+	err := ReadCSV(f, 1000)
 	if err != nil {
 		t.Errorf("ReadCSV() failed : %v", err)
 	}
 
-	// fmt.Println("Numero de claves: ", invertIndex.SizeKey())
-
-	start := time.Now()
-
-	list := invertIndex.Search("??")
-
-	if list == nil {
-		fmt.Println("Palara no encontrada")
-		return
-	}
-
-	// fmt.Println("Numero de canciones: ", list.Size())
-
-	for _, song := range list {
-		fmt.Println(song.TrackName, "id s", song.TrackId)
-	}
-
 	elapsed := time.Since(start)
 
-	// list.ForEach(func(song *s.Song, i int) {
-	// 	fmt.Println(i, song.TrackName, "id", song.TrackId)
-	// })
-
-	// fmt.Println("len", list.Size())
-
-	fmt.Println("Elapsed", elapsed.Microseconds(), "Microseconds")  // 0 microsegundos
-	fmt.Println("Numero de colisiones", invertIndex.GetColliding()) // 38 011 colisiones
+	fmt.Println("Elapsed", elapsed.Microseconds(), "Microseconds")
 }
